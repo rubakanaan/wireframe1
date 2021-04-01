444012e2d03ef4318e4b50336e62511b34538d49
root <root@STDLTUC-L069.localdomain>
2021-04-01 13:42:14 +0300

    add java script code

---
+++ app.js
@@ -0,0 +1,11 @@
+var userName = prompt("PLEASE ENTER YOUR NAME.","");
+       
+if (userName >= 'a' || userName >='A' )
+{
+  alert("WELCOME,"+userName+"!");
+} 
+else
+{ alert("WELCOME!");}
+confirm("PLEASE RATE OUR WEBSITE.");
+
+document.write("WELCOME TO RESTAURANT!")
